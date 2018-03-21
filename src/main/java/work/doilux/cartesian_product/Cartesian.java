9ffe05b56c75e2d0ae8eb88c4f8c9364f2940bb3
doilux <doilux0309@gmail.com>
2018-03-21 22:51:17 +0900

    Listを使うように変更

--- src/main/java/work/doilux/cartesian_product/Cartesian.java
+++ src/main/java/work/doilux/cartesian_product/Cartesian.java
@@ -2,33 +2,33 @@ package work.doilux.cartesian_product;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Cartesian {
 
-    private final Set<Object> l;
+    private final List<Object> l;
 
-    private Cartesian(Set<Object> l) {
+    private Cartesian(List<Object> l) {
         this.l = l;
     }
 
-    public static Cartesian of(Set<Object> l) {
+    public static Cartesian of(List<Object> l) {
         Objects.requireNonNull(l);
         return new Cartesian(l);
     }
 
-    public Set<Object> toSet() {
+    public List<Object> toList() {
         return l;
     }
 
-    public Set<Object> resolve(Set<Object> s2) {
+    public List<Object> multiply(List<Object> s2) {
         Objects.requireNonNull(s2);
-        return l.stream().map(s -> resolveOne(s, s2)).flatMap(Collection::stream).collect(Collectors.toSet());
+        return l.stream().map(s -> resolveOne(s, s2)).flatMap(Collection::stream).collect(Collectors.toList());
     }
 
-    private Set<ArrayList<Object>> resolveOne(Object n, Set<Object> s2) {
+    private List<ArrayList<Object>> resolveOne(Object n, List<Object> s2) {
         return s2.stream().map(s -> {
             if (n instanceof ArrayList) {
                 ArrayList<Object> l = (ArrayList<Object>) ((ArrayList<Object>) n).clone();
@@ -42,6 +42,6 @@ public class Cartesian {
                     }
                 };
             }
-        }).collect(Collectors.toSet());
+        }).collect(Collectors.toList());
     }
 }
--- src/test/groovy/work/doilux/cartesian_product/CartesianTest.groovy
+++ src/test/groovy/work/doilux/cartesian_product/CartesianTest.groovy
@@ -7,76 +7,68 @@ class CartesianTest extends Specification {
 
     def "multiple size"() {
         expect:
-        def act1 = Cartesian.of([1, 2] as Set).resolve(["a", "b"] as Set)
-
+        def act1 = Cartesian.of([1, 2]) * ["a", "b"]
         act1 == [
                 [1, "a"],
                 [1, "b"],
                 [2, "a"],
                 [2, "b"]
-        ] as Set
+        ]
 
-        def act2 = Cartesian.of(act1).resolve(["C", "D"] as Set)
-
+        def act2 = Cartesian.of(act1) * ["C", "D"]
         act2 == [
                 [1, "a", "C"],
-                [1, "b", "C"],
                 [1, "a", "D"],
+                [1, "b", "C"],
                 [1, "b", "D"],
                 [2, "a", "C"],
-                [2, "b", "C"],
                 [2, "a", "D"],
+                [2, "b", "C"],
                 [2, "b", "D"]
-        ] as Set
+        ]
     }
 
     def "s1 is one size"() {
         expect:
-        Cartesian.of([1] as Set).resolve(["a", "b"] as Set) == [
+        Cartesian.of([1]) * ["a", "b"] == [
                 [1, "a"],
                 [1, "b"]
-        ] as Set
+        ]
     }
 
     def "s2 is one size"() {
         expect:
-        Cartesian.of([1, 2] as Set).resolve(["a"] as Set) == [
+        Cartesian.of([1, 2]) * ["a"] == [
                 [1, "a"],
                 [2, "a"]
-        ] as Set
+        ]
     }
 
     def "s1, s2 is one size"() {
         expect:
-        Cartesian.of([1] as Set).resolve(["a"] as Set) == [
+        Cartesian.of([1]) * ["a"] == [
                 [1, "a"]
-        ] as Set
+        ]
     }
 
     def "s1 is zero size"() {
         expect:
-        Cartesian.of([] as Set).resolve(["a"] as Set) == [
-                []
-        ] as Set
+        Cartesian.of([]) * ["a"] == []
     }
 
     def "s2 is zero size"() {
         expect:
-        Cartesian.of([1] as Set).resolve([] as Set) == [
-                []
-        ] as Set
+        Cartesian.of([1]) * [] == []
     }
 
     def "s1, s2 is zero size"() {
         expect:
-        Cartesian.of([] as Set).resolve([] as Set) == [
-                []
-        ] as Set
+        Cartesian.of([]) * [] == []
     }
 
     def "s1 is null"() {
         when:
-        Cartesian.of(null).resolve([] as Set)
+        Cartesian.of(null) * []
 
         then:
         thrown(NullPointerException)
@@ -84,7 +76,7 @@ class CartesianTest extends Specification {
 
     def "s2 is null"() {
         when:
-        Cartesian.of([] as Set).resolve(null)
+        Cartesian.of([]) * null
 
         then:
         thrown(NullPointerException)
